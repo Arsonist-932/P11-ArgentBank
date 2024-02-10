dfa9daa60d9bdb5c7c86175302302435e73f6b60
Arsonist Tefh <119712506+Arsonist-932@users.noreply.github.com>
2024-02-10 03:19:43 +0100

    💄Add Files & Component
    - Create FormLogin
    - Create UserReducer
    - Create UserAction
    
    - Update main.jsx for add Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import App from "./App";
+import "./style/index.css";
+import rootReducer from "./reducers";
+
+// IMPORT REACT - REDUX
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./style/index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({ reducer: rootReducer });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar/Navbar";
+import Form from "../components/Form/Form";
 import Footer from "../components/Footer/Footer";
 
 function Login() {
@@ -7,7 +8,15 @@ function Login() {
       <header>
         <Navbar />
       </header>
-      <main></main>
+
+      <main className="main bg-dark login-content">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <Form />
+        </section>
+      </main>
+
       <footer className="footer">
         <Footer />
       </footer>
