43920a6dc207450db887fec8ab981130622813b8
Arsonist Tefh <119712506+Arsonist-932@users.noreply.github.com>
2024-02-15 15:15:17 +0100

    Add DevTools : false

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: false,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
